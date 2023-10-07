c94f61cbd96c8214626a5b91f85f8e6d85090195
VanyaLyashuk <ivan129522@gmail.com>
2023-10-07 14:48:58 +0300

    added: redux-persist

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -2,7 +2,7 @@ import FilterBtn from "./FilterBtn";
 
 const Filters = () => {
   return (
-    <div className="flex gap-1 sm:gap-2">
+    <div className="flex gap-[5px] sm:gap-2">
       <FilterBtn name="All" />
       <FilterBtn name="Todo" />
       <FilterBtn name="Completed" />
