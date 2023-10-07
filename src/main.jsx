f33fc018802eebd53cfad6eb81252220022eea56
VanyaLyashuk <ivan129522@gmail.com>
2023-10-07 14:49:41 +0300

    clean main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,10 @@ import './index.css';
 import { Provider } from 'react-redux';
 import store from './store';
 
-const todoList = [];
-const filters = [
-  {name: 'All', isActive: true},
-  {name: 'Todo', isActive: false},
-  {name:'Completed', isActive: false}
-];
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App todoList={todoList} filters={filters}/>
+      <App />
     </Provider>
   </React.StrictMode>,
 )
